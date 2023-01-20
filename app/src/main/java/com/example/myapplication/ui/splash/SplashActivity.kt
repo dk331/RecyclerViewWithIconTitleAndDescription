af694fb1ef6d4ec3681a5d223f077ac4ea99d36d
Dhananjay Kulkarni <dkulkarni331@gmail.com>
2023-01-21 01:58:22 +0530

    Setting desired image in Splash screen

--- app/src/main/java/com/example/myapplication/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/splash/SplashActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.ui.main.MainActivity
 
@@ -12,6 +14,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val imageView: ImageView = findViewById(R.id.image)
+        val drawableResourceId =
+            this.resources.getIdentifier("jerryadney", "drawable", this.packageName)
+
+        Glide.with(this).load(drawableResourceId).into(imageView)
+
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
